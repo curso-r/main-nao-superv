c82e04b7093c1222b8beae8c84be94d4ff75e4a3
Fernando <fptcorrea@gmail.com>
2022-09-05 14:31:58 -0300

    exemplo tnse

--- scripts/05-tnse.R
+++ scripts/05-tnse.R
@@ -8,3 +8,21 @@ library(tsne)
 # Dados -------------------------------------------------------------------
 
 
+multishapes <- factoextra::multishapes |>
+  tibble()
+
+multishapes |>
+  ggplot(aes(x = x, y = y, color = factor(shape))) +
+  geom_point()
+
+reducao_sne <- multishapes[,-3] |>
+  tsne(k = 2, perplexity = 50)
+
+reduzido = reducao_sne |>
+  as_tibble() |>
+  set_names(c("x", "y")) |>
+  mutate(shape = multishapes$shape)
+
+reduzido |>
+  ggplot(aes(x = x, y = y, color = factor(shape))) +
+  geom_point()
